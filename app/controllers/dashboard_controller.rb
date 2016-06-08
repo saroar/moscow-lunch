1791befa8e2726951ce493d164e4c889c01be14e
Saroar <saroar9@gmail.com>
2016-06-08 23:34:52 +0300

    fixed unsave after edit user account and create week days menu view and create method for week day menu

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def home
   end
+
+  def week_day_menu
+    @first_day = Date.today.at_beginning_of_week
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'dashboard#home'
   devise_for :users
+  root 'dashboard#home'
+  get 'week_day_menu' => 'dashboard#week_day_menu'
 end
