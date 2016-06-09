cd9cc92ee8ac93406529572e37c8b6911e4ed397
Saroar <saroar9@gmail.com>
2016-06-09 12:54:57 +0300

    config ActiveAdmin

---
+++ app/admin/user.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register User do
+  permit_params :name, :password, :password_confirmation
+
+  index do
+    column :id
+    column :email
+    column :name
+    column :admin
+    column :uid
+    column :sign_in_count
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def authenticate_active_admin_user!
+    authenticate_user!
+    unless current_user.has_role? :admin
+      flash[:notice] = "Sorry you are not Authorized to access this resource!"
+      redirect_to root_path
+    end
+  end
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) ||
       if current_user.has_role?(:admin)
-        root_path
+        admin_dashboard_path
       else
         week_day_menu_path
       end
