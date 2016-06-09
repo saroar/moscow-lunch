587e2088ca9e6902c6f157017e70bdc14feef367
Saroar <saroar9@gmail.com>
2016-06-09 13:22:28 +0300

    add missing paramas in ActiveAdmin user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :name, :password, :password_confirmation
+  permit_params :name, :email, :password, :password_confirmation
 
   index do
     column :id
