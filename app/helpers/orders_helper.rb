b95d6f4deabf7040d869aae8b0190ba6308bc0e2
Saroar <saroar9@gmail.com>
2016-06-10 19:51:28 +0300

    create helper method for check orders

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,5 @@
 module OrdersHelper
+  def had_order?
+    @orders.present? && (current_user.has_role? :admin) && (@date.to_date == Date.today) && (@orders.where(user: current_user).empty?)
+  end
 end
