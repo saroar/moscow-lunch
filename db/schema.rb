ff3654efea327e5d51a966790c85a31a08bcb3ea
Saroar <saroar9@gmail.com>
2016-06-13 16:17:37 +0300

    add auth_token to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612174131) do
+ActiveRecord::Schema.define(version: 20160613131611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20160612174131) do
     t.integer  "organization_id"
     t.string   "uid"
     t.string   "provider"
+    t.string   "auth_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
