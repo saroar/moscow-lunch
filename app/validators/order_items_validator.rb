1b59e8088f9e1da450549eacd6e87ceaff5c0b3c
Saroar <saroar9@gmail.com>
2016-06-13 13:18:07 +0300

    remove some extra validation

--- app/validators/order_items_validator.rb
+++ app/validators/order_items_validator.rb
@@ -1,30 +1,16 @@
 class OrderItemsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     begin
-      raise 'Items only Today\'s menu!' unless today_menu?(value)
       raise 'Order must be one item from each menu!' unless each_menu?(value)
-      raise 'Order only one item from each menu!' unless only_one_menu?(value)
     rescue Exception => e
       record.errors[attribute] << (options[:message] || e.message)
     end
   end
 
   private
-
-  def today_menu?(value)
-    menu_item_ids = DayMenu.actual(DateTime.now).items.map { |item| item.id }
-    order_item_ids = value.map { |item| item.id }
-    order_item_ids == (menu_item_ids && order_item_ids)
-  end
-
   def each_menu?(value)
     menu_ids = DayMenu.actual(DateTime.now).items.map { |item| item.menu.id }
     item_menu_ids = value.map { |item| item.menu.id }
     menu_ids.uniq == item_menu_ids.uniq
-  end
-
-  def only_one_menu?(value)
-    menu_ids = value.map { |item| item.menu.id }
-    menu_ids == menu_ids.uniq
   end
 end
