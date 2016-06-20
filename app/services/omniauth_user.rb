a8a4ff77b2ef13c7f12fbb90d76eaf4c2f3d9b26
Saroar <saroar9@gmail.com>
2016-06-20 17:32:38 +0300

    create new class for User#from_omniauth method and refactor

--- app/services/omniauth_user.rb
+++ app/services/omniauth_user.rb
@@ -6,27 +6,27 @@ class OmniauthUser
     @organization = organization
   end
 
-  def find_or_create(auth, organization)
-    if omniauth_user(auth, organization)
-      omniauth_user(auth, organization)
-    elsif registered_user(auth, organization)
-      registered_user(auth, organization)
+  def find_or_create
+    if omniauth_user
+      omniauth_user
+    elsif registered_user
+      registered_user
     else
-      create_omniauth_user(auth, organization)
+      create_omniauth_user
     end
   end
 
   private
 
-  def omniauth_user(auth, organization)
+  def omniauth_user
     User.where(provider: auth.provider, uid: auth.uid).first
   end
 
-  def registered_user(auth, organization)
+  def registered_user
     User.where(email: auth.info.email).first
   end
 
-  def create_omniauth_user(auth, organization)
+  def create_omniauth_user
     User.create(
        provider: auth.provider,
        uid: auth.uid,
